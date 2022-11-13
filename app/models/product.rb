46bd3e21a3fc69485eb770aa70e07af0629dc01f
Joey DeLizza <DeLizza1@stockton.go.edu>
2022-11-13 15:52:11 -0500

    Validate attributes

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,8 @@
 class Product < ApplicationRecord
   has_one_attached :pic
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :desc, presence: true
+  validates :pic, presence: true
+  
 end
