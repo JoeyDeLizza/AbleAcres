9249e069ec814c6ac52709f3c785c2317ca2a617
Joey DeLizza <DeLizza1@stockton.go.edu>
2022-11-14 00:04:20 -0500

    Validations for user creations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
-
+  validates :email, uniqueness: true
+  validates :email, format: {with: URI::MailTo::EMAIL_REGEXP}
+  validates :FirstName, :LastName, :email, :password, presence: true
   def welcome
     "Hello"
   end
