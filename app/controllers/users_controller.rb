ae84fa8f7883559c59ee03ba4d366c34b30cf152
minijarx <malonersr@gmail.com>
2022-11-28 15:09:58 -0500

    Edit Account Details Pt.2
    Routing of the form is all correct now, form errors reroute you back to the edit page, just need to update the user's attributes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,28 +38,33 @@ class UsersController < ApplicationController
     redirect_to request.path
   end
 
-<<<<<<< Updated upstream
+
   def current_user
     @user = User.find(session[:user_id])
   end
   helper_method :current_user
-=======
+
 
   
   def update
-    @user = User.create(user_params)
-    if @user.valid?
+    @current_user = User.create(params.require(:user).permit(:FirstName, :LastName))
+    if @current_user.valid?
       session[:user_id] = @user.id
       puts session[:user_id]
       puts @user.id
       current_user.FirstName=user.FirstName
+      current_user.LastName=user.LastName
       redirect_to users_path
     else
       flash[:error] = "Error- please try to edit account again."
-      redirect_to new_user_path
+      redirect_to edit_user_path
     end
   end
->>>>>>> Stashed changes
+
+  def edit
+    @current_user = current_user
+  end
+
 
   private 
   def user_params
