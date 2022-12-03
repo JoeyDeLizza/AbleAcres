d35a871b80de0d79a6279d7b26b225a1d7020473
Joe DeLizza <delizza1@go.stockton.edu>
2022-12-03 18:16:57 -0500

    Added admin attribute to session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,6 +10,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(email: session_params[:email])
     if @user && @user.authenticate(session_params[:password])
       session[:user_id] = @user.id
+      session[:admin] = @user.admin
       if @user.admin == true
         puts "Here"
         redirect_to admin_path
