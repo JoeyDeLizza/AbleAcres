86904f90f8a0e738d74adcaf009de6f4eb3940d0
Joe DeLizza <delizza1@go.stockton.edu>
2022-12-03 12:32:54 -0500

    Changed back to password_digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   validates :email, uniqueness: true
   validates :email, format: {with: URI::MailTo::EMAIL_REGEXP}
-  validates :FirstName, :LastName, :email, :password, presence: true
+  validates :FirstName, :LastName, :email, :password_digest, presence: true
   def welcome
     "Hello"
   end
