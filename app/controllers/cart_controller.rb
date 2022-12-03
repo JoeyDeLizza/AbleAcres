38593b0b03f4af460e2e72665577c08dbbd41706
Joe DeLizza <delizza1@go.stockton.edu>
2022-12-03 14:31:34 -0500

    checkout function that connectes with stripe api to fullfill orders

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -26,4 +26,34 @@ class CartController < ApplicationController
     add
     redirect_to "/cart"
   end
+
+  def checkout
+    puts "here"
+    # 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
+
+# This is your test secret API key.
+    Stripe.api_key = 'sk_test_51Lzm35C1N3KXwRbw2d1H52zmYhrGAPc6XqKYfg1l7c2bg8a5KntCCFseRfXbzBH3B53ToBLlhwL4suzb62amuThe00OpNrj0fO'
+    total = @cart.total
+    total = total.to_i
+    email = current_user.email
+
+    session = Stripe::Checkout::Session.create({
+    line_items: [{
+      # Provide the exact Price ID (e.g. pr_1234) of the product you want to sell
+                   price_data: {
+                     currency: "usd",
+                     product_data: {
+                       name: "Order"},
+                     unit_amount: total*100},
+                   quantity: 1
+                 }],
+    mode: 'payment',
+    customer_email: email,
+    success_url: "http://www.google.com",
+    cancel_url: "http://www.bing.com",
+    automatic_tax: {enabled: false},
+                                               })
+    puts session.url
+    redirect_to session.url, allow_other_host: true
+  end
 end
